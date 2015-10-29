006b8c0247b52680e3dcc77c29e89f9798f2e559
kumarsarath588 <kumar.sarath588@gmail.com>
2015-10-29 17:36:30 +0530

    Added elb variables

--- guacamole/guacamole.sh
+++ guacamole/guacamole.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "<VirtualHost *:80>
-        ServerName 192.168.4.221
-        ProxyPass /guacamole/ http://192.168.4.221:8080/guacamole/
-        ProxyPassReverse /guacamole/ http://192.168.4.221:8080/guacamole/
+        ServerName ${elb}
+        ProxyPass /guacamole/ http://${elb}:8080/guacamole/
+        ProxyPassReverse /guacamole/ http://${elb}:8080/guacamole/
         ProxyRequests Off
         ProxyPreserveHost on
         ProxyBadHeader Ignore
 
-        <Proxy http://192.168.4.221:8080/*>
+        <Proxy http://${elb}:8080/*>
         Order deny,allow
         Allow from all
         </Proxy>
